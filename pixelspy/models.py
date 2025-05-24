92f72540f026ee296b6c715dbbb9b3f90c541844
ilwan <miloudilwan@gmail.com>
2025-05-24 19:28:28 +0200

    updated models

--- pixelspy/models.py
+++ pixelspy/models.py
@@ -1,4 +1,6 @@
+import django
 from django.db import models
+from django.utils import timezone
 import datetime
 
 # Create your models here.
@@ -8,12 +10,26 @@ class Pixel(models.Model):
     """
     creation_date = models.DateTimeField("date of creation")  # date of creation of the image object
     life_duration = models.DurationField("life duration", default=datetime.timedelta(0))  # time for which the image will exist (0 for unlimited)
-    user_owner = models.CharField("pixel owner")  # username of the owner of this image
+    display_url = models.URLField("pixel display url", default=None)  # url to use for requesting the image
+    user_owner = models.CharField("pixel owner")  # username of the owner of this image  #TODO: point to user model directly
+
+    def __str__(self):
+        if self.display_url is not None:
+            return self.display_url.split("/")[-1]
+        else:
+            return super().__str__()
+    
+    def is_expired(self) -> bool:
+        return self.life_duration >= timezone.now() - self.creation_date
 
 class Tracker(models.Model):
     """
-    a model to store the informations when a mail has been read
+    a model to store the informations when a mail has been read once, a pixel can have multiple trackers
     """
     pixel = models.ForeignKey(Pixel, on_delete=models.CASCADE)  # the pixel from which the tracking originates
     opened_date = models.DateField("date of opening")  # date for which the mail was opened
     ip = models.GenericIPAddressField("receiver ip")  # ip address of the mail receiver
+    request_header = models.CharField("request header metadata", default="")  # full content of the request header
+
+    def __str__(self):
+        return f"{str(self.opened_date)} from {self.ip}"
