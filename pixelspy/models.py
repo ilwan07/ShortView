33d564a9bd63987e339e8c480460c599f96d0791
ilwan <miloudilwan@gmail.com>
2025-05-31 21:19:52 +0200

    cleaned models a bit

--- pixelspy/models.py
+++ pixelspy/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 import datetime
 
 # Create your models here.
@@ -8,18 +9,18 @@ class Pixel(models.Model):
     a model to represent a tracking pixel with its attributes
     """
     creation_date = models.DateTimeField("date of creation")  # date of creation of the image object
-    life_duration = models.DurationField("life duration", default=datetime.timedelta(0))  # time for which the image will exist (0 for unlimited)
-    display_url = models.URLField("pixel display url", default=None)  # url to use for requesting the image
-    user_owner = models.CharField("pixel owner")  # username of the owner of this image  #TODO: point to user model directly
+    lifetime = models.DurationField("life duration", default=datetime.timedelta(0))  # time for which the image will exist (0 for unlimited)
+    url = models.URLField("pixel display url", default=None)  # url to use for requesting the image
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)  # owner of this image
 
     def __str__(self):
-        if self.display_url is not None:
-            return self.display_url.split("/")[-1]
+        if self.url is not None:
+            return self.url.split("/")[-1]
         else:
             return super().__str__()
     
     def is_expired(self) -> bool:
-        return self.life_duration >= timezone.now() - self.creation_date
+        return self.lifetime >= timezone.now() - self.creation_date
 
 
 class Tracker(models.Model):
@@ -27,9 +28,9 @@ class Tracker(models.Model):
     a model to store the informations when a mail has been read once, a pixel can have multiple trackers
     """
     pixel = models.ForeignKey(Pixel, on_delete=models.CASCADE)  # the pixel from which the tracking originates
-    opened_date = models.DateField("date of opening")  # date for which the mail was opened
+    date = models.DateField("date of opening")  # date for which the mail was opened
     ip = models.GenericIPAddressField("receiver ip")  # ip address of the mail receiver
-    request_header = models.CharField("request header metadata", default="")  # full content of the request header
+    header = models.CharField("request header metadata", default="")  # full content of the request header
 
     def __str__(self):
-        return f"{str(self.opened_date)} from {self.ip}"
+        return f"{str(self.date)} from {self.ip}"
