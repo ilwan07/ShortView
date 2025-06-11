2f3642e0225fc479975507054c44a71ff98cfe2c
ilwan <miloudilwan@gmail.com>
2025-06-11 19:38:58 +0200

    added password explaination and changed hashing algorithm

--- website/settings.py
+++ website/settings.py
@@ -80,6 +80,15 @@ DATABASES = {
     }
 }
 
+# Password hashing functions
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    #"django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+    #"django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    #"django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    #"django.contrib.auth.hashers.ScryptPasswordHasher",
+]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
